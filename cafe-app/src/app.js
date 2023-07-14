0eb339c1f2076bcbeaaea63fdffc74ef1f16213b
Luis Miguel <lmavendano1597@gmail.com>
2023-07-13 21:42:47 -0500

    clase #4 - parte 1

--- cafe-app/src/app.js
+++ cafe-app/src/app.js
@@ -1,44 +1,39 @@
 const express = require("express");
 
+//routes
+const productRouter = require("./routes/products.route");
+
 const app = express();
 
 app.use(express.json());
 
-const findProducts = (req, res) => {
-  res.status(200).json({
-    message: "hello from the get route!",
-  });
-};
+//!desde aqui
 
-//? Buscar Varios Productos
-app.get("/api/v1/products", findProducts);
-
-//*createProduct
-//? Crear un producto
-app.post("/api/v1/products", (req, res) => {
-  const product = req.body;
-
-  res.status(201).json({
-    message: "hello from de post route",
-    product,
-  });
-});
-
-//*findProduct
-//? Buscar un producto dado un id
-app.get("/api/v1/products/:id", (req, res) => {
+const updateProduct = (req, res) => {
   const id = req.params.id;
 
   res.status(200).json({
-    message: "hello from the get route with id",
+    message: "hello from the update route with id",
     id,
   });
-});
+};
 
-//*updatedProduct
-//? Actualizar un producto
-//*deleteProduct
-//? Eliminar un producto
+const deleteProduct = (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    ok: true,
+    parametros: req.params,
+  });
+};
+
+app.patch("/api/v1/products/:id", updateProduct);
+
+app.delete("/api/v1/products/:id", deleteProduct);
+
+//!hasta aqui
+
+app.use("/api/v1/products", productRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
