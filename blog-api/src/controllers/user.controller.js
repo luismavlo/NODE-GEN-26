8ac5be3f51812d39b924d0895e49f8e233736c99
Luis Miguel <lmavendano1597@gmail.com>
2023-07-21 22:00:14 -0500

    next commit

--- blog-api/src/controllers/user.controller.js
+++ blog-api/src/controllers/user.controller.js
@@ -63,21 +63,7 @@ exports.updateUser = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
   try {
-    const { id } = req.params;
-
-    const user = await User.findOne({
-      where: {
-        id,
-        status: 'active',
-      },
-    });
-
-    if (!user) {
-      return res.status(404).json({
-        status: 'error',
-        message: `User with id: ${id} not found`,
-      });
-    }
+    const { user } = req;
 
     await user.update({ status: 'inactive' });
 
