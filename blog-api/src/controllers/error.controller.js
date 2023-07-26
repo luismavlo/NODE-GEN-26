bc2fde09d0966f23073f0a6b0e2eefe1d94bf835
Luis Miguel <lmavendano1597@gmail.com>
2023-07-25 21:01:52 -0500

    next commit

--- blog-api/src/controllers/error.controller.js
+++ blog-api/src/controllers/error.controller.js
@@ -1,3 +1,8 @@
+const AppError = require('./../utils/appError');
+
+const handleCastError22001 = () =>
+  new AppError('The number of characters is greater than expected', 400);
+
 const sendErrorDev = (err, res) => {
   console.log(err);
   return res.status(err.statusCode).json({
@@ -6,6 +11,23 @@ const sendErrorDev = (err, res) => {
     stack: err.stack,
     err: err,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  console.log(err);
+  //operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //programming or other unknown error: don't leak error detail
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong!',
+    });
+  }
 };
 
 const globalErrorHander = (err, req, res, next) => {
@@ -17,6 +39,10 @@ const globalErrorHander = (err, req, res, next) => {
   }
 
   if (process.env.NODE_ENV === 'production') {
+    let error = err;
+    if (err.parent?.code === '22001') error = handleCastError22001();
+
+    sendErrorProd(error, res);
   }
 };
 
