2b41d835dcae99ff3c5b11c88d5f7b2ea303bca6
Luis Miguel <lmavendano1597@gmail.com>
2023-08-03 22:31:03 -0500

    next commit

--- blog-api/src/app.js
+++ blog-api/src/app.js
@@ -1,6 +1,10 @@
 const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const sanitizater = require('perfect-express-sanitizer');
 
 const AppError = require('./utils/appError');
 const globalErrorHander = require('./controllers/error.controller');
@@ -12,14 +16,29 @@ const postRoutes = require('./routes/post.route');
 const commentRoutes = require('./routes/comment.route');
 
 const app = express();
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(hpp());
+app.use(
+  sanitizater.clean({
+    xss: true,
+    noSql: true,
+    sql: false, //obligatoriamente debe ir en false
+  })
+);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use('/api/v1', limiter);
 //routes
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/auth', authRoutes);
--- blog-api/src/middlewares/post.middleware.js
+++ blog-api/src/middlewares/post.middleware.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utils/catchAsync');
 const { Post, postStatus } = require('../models/post.model');
 const AppError = require('../utils/appError');
+const User = require('../models/user.model');
 
 exports.validPost = catchAsync(async (req, res, next) => {
   //1. traer info de la req.params
@@ -12,12 +13,19 @@ exports.validPost = catchAsync(async (req, res, next) => {
       status: postStatus.active,
       id,
     },
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'name', 'profileImgUrl', 'description'],
+      },
+    ],
   });
   //3. valido que el post exista
   if (!post) {
     return next(new AppError(`Post with id: ${id} not found`, 404));
   }
   //4. adjunto el post por la req. y doy continuidad
+  req.user = post.user;
   req.post = post;
   next();
 });
